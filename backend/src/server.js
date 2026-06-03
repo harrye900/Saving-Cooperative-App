40b911806bccd28079f3b5ddafbf9420982d32c7
harrye900 <harrye900@gmail.com>
2026-06-03 14:45:42 -0400

    Fix CORS for mobile

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,10 +9,10 @@ const { sendReminders } = require('./services/reminders');
 const app = express();
 
 // Security
-app.use(helmet({ contentSecurityPolicy: false }));
-app.use(cors());
+app.use(helmet({ contentSecurityPolicy: false, crossOriginEmbedderPolicy: false }));
+app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE'], allowedHeaders: ['Content-Type', 'Authorization'] }));
 app.use(express.json());
-app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
+app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 200 }));
 
 // Serve Flutter web app
 const path = require('path');
